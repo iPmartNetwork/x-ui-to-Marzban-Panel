049fa35992eb3a35020f21a7055d607266a3964b
ali (zeus) <131793522+iPmartNetwork@users.noreply.github.com>
2024-05-04 00:57:04 +0330

    Create config.py

---
+++ config.py
@@ -0,0 +1,18 @@
+# Define Variables for Both Panels
+
+# X-Ui Panel
+
+X_DOMAIN = "YOUR_DOMAIN"
+X_PORT = "YOUR_PORT"
+X_USERNAME = "YOUR_USERNAME"
+X_PASSWORD = "YOUR_PASSWORD"
+X_HTTPS = False  # Set to True to use HTTPS, False to use HTTP
+X_FORK = 1 # Set to 1 if you have MHSanaie, Set to 2 if you have Alireza (Default: MHSanaie)
+
+# Marzban Panel
+
+M_DOMAIN = "YOUR_DOMAIN"
+M_PORT = "YOUR_PORT"
+M_USERNAME = "YOUR_USERNAME"
+M_PASSWORD = "YOUR_PASSWORD"
+M_HTTPS = False  # Set to True to use HTTPS, False to use HTTP
